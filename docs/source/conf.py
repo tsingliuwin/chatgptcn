177dbe3e269fdd74f7e83c60cf244d1aaadc1123
Tsingliu <410869548@qq.com>
2023-03-26 00:48:29 +0000

    修改配置

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ChatGPT中文文档'
+copyright = '2023, Tsingliu'
+author = 'Tsingliu'
 
 release = '0.1'
 version = '0.1.0'
