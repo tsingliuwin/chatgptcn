d247006949715370936f1874ed952b087c10f4eb
Tsingliu <410869548@qq.com>
2023-03-26 02:20:00 +0000

    mkdocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,13 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'myst_parser'
 ]
-
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
-}
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
