daabaf93c153a47defbedbb6a601ac069652e7e9
tsingliu <410869548@qq.com>
2023-08-03 16:34:14 +0800

    增加静态文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,7 +8,7 @@ export default defineUserConfig({
   description: 'ChatGPT, LLM, 大模型, 大语言模型, ChatGPT4.0, ChatGPT3.5',
   head: [
     ['meta', {name: 'google-site-verification', content: 'mbC3Y2iTQfTAjqFGZb_eK116Rol1RmRMJTpY8Y8FN5s'}],
-    ['meta', {name: 'baidu-site-verification', content: 'codeva-x6aGkEgMlp'}]
+    // ['meta', {name: 'baidu-site-verification', content: 'codeva-x6aGkEgMlp'}]
   ],
   plugins: [
     googleAnalyticsPlugin({
