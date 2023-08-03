4ef6a4050e5df168091db333090f25229233058c
tsingliu <410869548@qq.com>
2023-08-03 13:36:41 +0800

    完成迁移

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,6 +20,130 @@ export default defineUserConfig({
         {
             text: 'OpenAI',
             link: '/openai/'
+        },
+        {
+          text: '官方文档',
+          link: '/documents/',
+          children: [
+            {
+              text: '介绍',
+              link: '/documents/introduction/'
+            },
+            {
+              text: '体验',
+              link: '/documents/knows/'
+            },
+            {
+              text: '快速开始',
+              link: '/documents/quickstart/'
+            }
+          ]
+        },
+        {
+          text: 'System prompt合集',
+          link: '/prompt/',
+          children: [
+            {
+              text: '中文合集',
+              link: '/prompt/system_prompt_cn/'
+            },
+            {
+              text: '英文合集',
+              link: '/prompt/system_prompt_en/'
+            },
+            {
+              text: '特殊风格合集',
+              link: '/prompt/system_prompt_other/'
+            }
+          ]
+        },
+        {
+          text: 'Hugging Face',
+          link: '/huggingface/',
+          children: [
+            {
+              text: '介绍',
+              link: '/huggingface/introduction/'
+            }
+          ]
+        },
+        {
+          text: '开源大模型',
+          link: '/opensource/',
+          children: [
+            {
+              text: 'ChatGLM2-6B',
+              link: '/opensource/ChatGLM-6B/'
+            }
+          ]
+        },
+        {
+          text: '中文测试集',
+          link: '/testset/'
+        },
+        {
+          text: '其它GPT',
+          link: '/other/',
+          children: [
+            {
+              text: '文心一言',
+              link: '/other/wenxin/'
+            },
+            {
+              text: '通义千问',
+              link: '/other/qianwen/'
+            },
+            {
+              text: '天工助手',
+              link: '/other/tiangong/'
+            },
+            {
+              text: '讯飞星火',
+              link: '/other/xinghuo/'
+            }
+          ]
+        },
+        {
+          text: '应用',
+          link: '/application/',
+          children: [
+            {
+              text: '5movies',
+              link: '/application/5movies/'
+            }
+          ]
+        },
+        {
+          text: '转载',
+          link: '/transport/',
+          children: [
+            {
+              text: 'ChatGPT基础科普',
+              link: '/transport/basic/'
+            },
+            {
+              text: '陆奇：我的大模型世界观',
+              link: '/transport/my_llm/'
+            }
+          ]
+        },
+        {
+          text: '自然语言处理',
+          link: '/nlp/',
+          children: [
+            {
+              text: 'HanLP',
+              link: '/nlp/hanlp/'
+            }
+          ]
+        },
+        {
+          text: '关于',
+          link: '/about/'
+        },
+        {
+          text: '公告',
+          link: '/notice/'
         }
     ]
   })
